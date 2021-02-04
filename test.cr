4a6176ec69edb60f39328136ef0fb67b277593c3
Sean Gregory <sean.christopher.gregory@gmail.com>
2021-02-04 07:34:11 -0500

    example

--- test.cr
+++ test.cr
@@ -2,5 +2,5 @@ require "./src/audio"
 
 puts "hello"
 Audio.init
-num_of_devices = Audio::Device.scan.map &.name
-puts "devices: #{num_of_devices.join(",")}"
+devices = Audio::Device.scan.map &.name
+puts "devices: #{devices.join(",")}"
